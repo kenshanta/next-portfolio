286d2db55d5070a4f312b9095219398bff41283e
Sarkis Kovlekjian <kovlekjian@Sarkiss-MacBook-Pro.local>
2023-09-09 17:33:30 +0200

    remove error filled actions

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,30 +3,25 @@ import db from '@/utils/db'
 import { revalidatePath } from 'next/cache'
 
 export const newTodo = async (data: FormData) => {
-  const newTodo = data.get('todo') as string
+  // const newTodo = data.get('todo') as string
 
-  const commonData = await db.todo.findFirst({
-    where: {
-      content: newTodo,
-    },
-  })
-  if (newTodo && !commonData) {
-    await db.todo.create({
-      data: {
-        content: newTodo,
-      },
-    })
+  // if (newTodo &&) {
+  //   await db.todo.create({
+  //     data: {
+  //       content: newTodo,
+  //     },
+  //   })
 
-    revalidatePath('/todos')
-  }
+  // }
+  revalidatePath('/todos')
 }
 
 export const completeTodo = async (id?: string) => {
-  await db.todo.update({
-    where: { id },
-    data: {
-      completed: true,
-    },
-  })
+  // await db.todo.update({
+  //   where: { id },
+  //   data: {
+  //     completed: true,
+  //   },
+  // })
   revalidatePath('/todos')
 }
