49446597b0e5187e05542f665239ca3eff687746
Sarkis Kovlekjian <kovlekjian@Sarkiss-MacBook-Pro.local>
2023-09-10 10:18:46 +0200

    updating db + package json

--- prisma/migrations/20230905192650_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "completed" BOOLEAN NOT NULL DEFAULT false,
-    "content" TEXT NOT NULL DEFAULT ''
-);
