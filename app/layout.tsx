5d0401324b0190992691cc0e1b97f3d4472436c0
Sarkis Kovlekjian <kovlekjian@Sarkiss-MacBook-Pro.local>
2023-11-08 20:12:04 +0100

    update Head and meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Theme } from '@radix-ui/themes'
 import './theme-config.css'
 import '@radix-ui/themes/styles.css'
+import Head from 'next/head'
 
 export const metadata = {
   title: `kovlekjian`,
@@ -14,11 +15,14 @@ export const metadata = {
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <title>Sarkis Kovlekjian</title>
-      <meta
-        name="kovlekjian"
-        content="A turbo-chatgpt-3.5 backed; customized with personal answers related to the field of question and it's legitimacy to my professional career. Still under construction"
-      ></meta>
+      <Head>
+        <title>Sarkis Kovlekjian&apos;s Portfolio</title>
+        <meta
+          name="kovlekjian"
+          content="A turbo-chatgpt-3.5 backed, customized with personal answers. Still under construction"
+        ></meta>
+      </Head>
+
       <ClerkProvider>
         <body>
           <Theme radius="large" appearance="inherit">
